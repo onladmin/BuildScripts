b15f916557f1c57cf8f3b31b7f0b1e8c7f325151
ONLMo <78916125+ONLMo@users.noreply.github.com>
2021-02-11 15:39:10 +0000

    Add office install script for automation

---
+++ Scripts/Automation Scripts/auto-install-office.ps1
@@ -0,0 +1,21 @@
+$OfficeExe = 'https://github.com/Ray-MRQ/MRQ/raw/master/Install%20files/setup.exe'
+$OfficeXMLInstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xmls/configuration-Office365-x86.xml'
+$OfficeXMLUninstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xmls/configruation_uninstall.xml'
+$OfficeXMLHomeUninstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xmls/configuration_uninstall_home.xml'
+$OfficeXMLBuisnessUninstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xmls/configuration_uninstall_buisness.xml'
+
+function start-officeinstall {
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $OfficeExe -outfile c:\temp\scriptdownloads\office365setup.exe
+    Invoke-WebRequest $OfficeXMLHomeUninstall -outfile c:\temp\scriptdownloads\office365uninstallhome.xml
+    Invoke-WebRequest $OfficeXMLInstall -outfile c:\temp\scriptdownloads\configuration.xml
+    Invoke-WebRequest $OfficeXMLUninstall -outfile c:\temp\scriptdownloads\office365uninstall.xml
+    Invoke-WebRequest $OfficeXMLBuisnessUninstall -outfile c:\temp\scriptdownloads\office365uninstallbuisness.xml
+    $ProgressPreference = 'Continue'
+    c:\temp\scriptdownloads\office365setup.exe /configure c:\temp\scriptdownloads\office365uninstallhome.xml | Out-Null
+    c:\temp\scriptdownloads\office365setup.exe /configure c:\temp\scriptdownloads\office365uninstallbuisness.xml | Out-Null
+    c:\temp\scriptdownloads\office365setup.exe /configure c:\temp\scriptdownloads\office365uninstall.xml | Out-Null
+    c:\temp\scriptdownloads\office365setup.exe /configure c:\temp\scriptdownloads\configuration.xml | Out-Null
+    Write-Output "Office365 is now installed."
+}
+
